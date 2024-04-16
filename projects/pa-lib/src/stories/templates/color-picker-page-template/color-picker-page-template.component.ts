5d712d22f57972b31008644d9259539fc9ea090d
norita <kyohei.norita@gmail.com>
2024-04-16 22:01:13 +0900

    ColorPickerPageTemplateComponentを修正

--- projects/pa-lib/src/stories/templates/color-picker-page-template/color-picker-page-template.component.ts
+++ projects/pa-lib/src/stories/templates/color-picker-page-template/color-picker-page-template.component.ts
@@ -1,8 +1,12 @@
-import {Component, EventEmitter, Output, signal} from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {Component, EventEmitter, Input, Output, signal} from '@angular/core';
+import {CommonModule} from '@angular/common';
 import {FileInputButtonComponent} from "../../atoms/file-input-button/file-input-button.component";
 import {PixelColorPickerComponent} from "../../molecules/pixel-color-picker/pixel-color-picker.component";
 import {formatAsRgb, RGB, toRGB} from "../../../lib/rgb";
+
+export type ColorPickerPageTemplateInput = {
+  pageTitle: string,
+}
 
 @Component({
   selector: 'pal-color-picker-page-template',
@@ -12,6 +16,7 @@ import {formatAsRgb, RGB, toRGB} from "../../../lib/rgb";
   styleUrls: ['./color-picker-page-template.component.css']
 })
 export class ColorPickerPageTemplateComponent {
+  @Input({required: true}) input!: ColorPickerPageTemplateInput;
   @Output() onColorPick = new EventEmitter<RGB>();
   readonly dataUrl = signal<string | undefined>(undefined)
   readonly selectedColor = signal<string>('');
--- projects/pa-lib/src/stories/templates/color-picker-page-template/color-picker-page-template.stories.ts
+++ projects/pa-lib/src/stories/templates/color-picker-page-template/color-picker-page-template.stories.ts
@@ -1,24 +1,61 @@
-import {argsToTemplate, Meta, StoryObj} from '@storybook/angular';
+import {argsToTemplate, Meta, moduleMetadata, StoryObj} from '@storybook/angular';
 import {ColorPickerPageTemplateComponent} from "./color-picker-page-template.component";
-
+import {Table} from "../../organisms/table/table.component";
+import * as PaintTablePageTemplate  from "../paint-table-page-template/paint-table-page-template.stories";
+import {
+  PaintTablePageTemplateComponent,
+  PaintTablePageTemplateInput
+} from "../paint-table-page-template/paint-table-page-template.component";
 const meta: Meta<ColorPickerPageTemplateComponent> = {
   title: 'Templates/ColorPickerPageTemplate',
   component: ColorPickerPageTemplateComponent,
   tags: ['autodocs'],
+  decorators: [
+    moduleMetadata({
+      imports: [
+        PaintTablePageTemplateComponent,
+      ],
+    }),
+  ],
 };
 
 export default meta;
-type Story = StoryObj<ColorPickerPageTemplateComponent>;
+type ColorPickerPagePropsAndOtherPageProps = ColorPickerPageTemplateComponent & {
+  paintTablePageInput: PaintTablePageTemplateInput
+}
+type Story = StoryObj<ColorPickerPagePropsAndOtherPageProps>;
+
+export const Default: Story = {
+  render: ({...args}) => ({
+    props: args,
+    template: `
+      <pal-color-picker-page-template ${argsToTemplate(args)}>
+        ng-content
+      </pal-color-picker-page-template>
+    `
+  }),
+  args: {
+    input: {
+      pageTitle: 'Title',
+    }
+  },
+};
 
 export const Example: Story = {
   render: ({...args}) => ({
     props: args,
     template: `
-      <pal-color-picker-page-template ${argsToTemplate(args)}/>
+      <pal-color-picker-page-template ${argsToTemplate(args)}>
+        <pal-paint-table-page-template [input]="paintTablePageInput"/>
+      </pal-color-picker-page-template>
     `
   }),
   args: {
-
+    input: {
+      pageTitle: 'Title',
+    },
+    paintTablePageInput: PaintTablePageTemplate.Example.args?.input
   },
 };
+
 
